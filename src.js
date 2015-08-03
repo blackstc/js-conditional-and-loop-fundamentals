f5a06ddb5525983394c2906e1debdf478b94db4b
Blackstock <charlieblackstock@gmail.com>
2015-08-03 10:48:12 -0600

    added functional JS solutions to all test suites

--- src.js
+++ src.js
@@ -18,6 +18,40 @@ var yourCode = {
       results += "[" + i + "]";
     }
     return results;
+  },
+
+  sumOfN: function(num) {
+    var total = 0;
+    for (var i = 1; i <= num; i++) {
+      total += i;
+    }
+    return total;
+  },
+
+  moviesFor: function(actor) {
+    switch (actor) {
+      case "Robin Williams":
+        return "Jumanji, Hook, Good Will Hunting";
+      case "Sandra Bullock":
+        return "Gravity, The Lake House, The Blind Side";
+      case "Daniel Radcliffe":
+        return "All of the Harry Potters";
+      default:
+        return "Please provide an actor or actress";
+    }
+  },
+
+  joinWithForAndIndex: function(arr) {
+    var result = "";
+    for (var i = 0; i < arr.length; i++) {
+      result += arr[i] + i;
+    }
+    return result;
+  },
+
+  joinWithForAndToken: function(arr, token) {
+    var results = arr.join(token);
+    return results;
   }
 };
 
